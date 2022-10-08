25c5ed293db20cfbc56e926a15d374abc045a06d
Wiliender <wiliendersilva@gmail.com>
2022-10-08 03:26:48 -0300

    Feat: Register route healthcheck

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+from routes import shipping, healthcheck
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
-
+    
+    # register routes
+    app.register_blueprint(healthcheck.route)
+    
     return app
 
 if __name__ == '__main__':
