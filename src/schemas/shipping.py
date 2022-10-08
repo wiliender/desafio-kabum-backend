0d180c6a7da6a101c6d070e4d8bf7d3932772467
Wiliender <wiliendersilva@gmail.com>
2022-10-08 17:42:04 -0300

    Update Schema Class required

--- src/schemas/shipping.py
+++ src/schemas/shipping.py
@@ -1,9 +1,9 @@
 from marshmallow import Schema, fields
 
 class Dimension(Schema):
-    width = fields.Float()
-    height = fields.Float()
+    width = fields.Float(required=True)
+    height = fields.Float(required=True)
 
 class Payload(Schema):
-    dimension = fields.Nested(Dimension)
-    weight = fields.Float()
+    dimension = fields.Nested(Dimension, required=True)
+    weight = fields.Float(required=True)
