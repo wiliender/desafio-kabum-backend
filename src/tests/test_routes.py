8649ccfc000f64b52182f3bb4e5fa6a55063d680
Wiliender <wiliendersilva@gmail.com>
2022-10-09 17:17:04 -0300

    Update test route

--- src/tests/test_routes.py
+++ src/tests/test_routes.py
@@ -1,13 +1,13 @@
 from httpx import post, get
+from schemas.shipping import Payload
 
 url_healthcheck = "http://localhost:5000/"
-url_shipping = "http://localhost:5000/shipping"
+url_shipping = "http://localhost:5000/shipping/"
 
 def test_get_healthcheck_200():
     request = get(url_healthcheck)
     assert request.status_code == 200
 
-def test_post_shipping_201():
+def test_post_shipping_missing_information_body_400():
     request = post(url_shipping)
-    assert request.status_code == 308 or 201
-    
+    assert request.status_code == 400 
